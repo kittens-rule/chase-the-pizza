5457356b3d42ac42f441f51ab26ec22be0db8a9c
kittens-rule <alexzimolzakmoulton@outlook.com>
2025-09-02 07:10:07 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    pizza.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+    info.startCountdown(30)
+})
+let pizza: Sprite = null
+scene.setBackgroundColor(13)
+let mySprite = sprites.create(img`
+    . . . . c c c c c c . . . . . . 
+    . . . c 6 7 7 7 7 6 c . . . . . 
+    . . c 7 7 7 7 7 7 7 7 c . . . . 
+    . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+    . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+    . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+    . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+    . . f 7 7 7 7 6 c 7 7 6 f c . . 
+    . . . f c c c c 7 7 6 f 7 7 c . 
+    . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+    . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+    c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+    f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+    . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+    . . c c c c c c c c c f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+pizza = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . 3 . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 5 5 5 5 5 4 e 4 4 b . 
+    . . . b d 5 5 d 5 5 5 4 e 4 4 e 
+    . . . b 5 5 d d 5 5 5 5 e e e e 
+    . . b d 5 5 5 5 5 5 5 5 5 e e e 
+    . . b 5 d 5 5 5 5 5 d 5 5 d e e 
+    . b 5 5 5 5 d 5 5 d 5 5 d d e 4 
+    . b 5 5 5 d 5 5 5 5 5 d d e 4 . 
+    b d 5 d 5 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Food)
